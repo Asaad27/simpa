a5b971324badef74981b9f5b69df3a8b958637cb
Karim Hossen <karim.hossen@imag.fr>
2012-09-04 09:55:33 +0200

    api ready

--- hit/src/drivers/web/WebDriver.java
+++ hit/src/drivers/web/WebDriver.java
@@ -11,6 +11,28 @@ import automata.efsm.ParameterizedInputSequence;
 import automata.efsm.ParameterizedOutput;
 import drivers.efsm.EFSMDriver;
 
+/*
+ * Driver - Help
+ * 
+ * The driver class must extend driver.web.WebDriver.
+ * The others classes is reserved for testing or other algorithms (not used in SPaCIoS)
+ * 
+ * For an example of a complete class see WGStoredXSSDriver.java
+ * 
+ * The contructor defines the ip address and port to access the system
+ * Then it can calls initConnection to be able to connect to the system (Basic auth for Webgoat)
+ * 
+ * getOutputSymbols / getInputSymbols functions returns a list of symbols (String)
+ * 
+ * Each symbols have at least one parameter, the default values is specified by the getDefaultParamValues function.
+ * And their name is defined in the getParameterNames function.
+ * 
+ * abstractToConcrete take ParameterizedInput (one symbol and its parameter) as input and return the corresponding HTTP request
+ * 
+ * concreteToAbstract take HTTPResponse as input and return the corresponding abstract response (one symbol and its parameter)
+ *  
+ */
+
 public abstract class WebDriver extends EFSMDriver{
 	public String systemHost;
 	public int systemPort;
--- hit/src/main/Test.java
+++ hit/src/main/Test.java
@@ -1,37 +1,8 @@
 package main;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-
-import learner.efsm.LiConjecture;
-import tools.ASLanEntity;
-import tools.Utils;
-import tools.loggers.LogManager;
-
 public class Test {
-
 	public static void main(String[] args) {
-		LiConjecture c = LiConjecture.deserialize("saved_efsm");
-		ASLanEntity e = new ASLanEntity("bob");
-		e.loadFromEFSM(c);
-		Writer writer = null;
-		File file = null;
-		try {
-			File dir = new File(Options.OUTDIR + File.separator + Options.DIRASLAN);		
-			if (Utils.createDir(dir)){
-				file = new File(dir.getPath() + File.separator + "bob" + ".aslan++");				
-				writer = new BufferedWriter(new FileWriter(file));				
-				writer.write(e.toString());								
-				if (writer != null) writer.close();
-				LogManager.logInfo("Conjecture have been exported to " + file.getName());
-			}else
-				LogManager.logError("unable to create "+ dir.getName() +" directory");
-		} catch (IOException d) {
-			d.printStackTrace();
-		}		
-    }
+		
+	}
 
 }
