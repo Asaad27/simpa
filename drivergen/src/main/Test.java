4138aeb50d15ed11608fd1f542ab1a1c53fdc637
Karim Hossen <karim.hossen@imag.fr>
2012-11-07 21:43:22 +0100

    Separated crawler

---
+++ drivergen/src/main/Test.java
@@ -0,0 +1,12 @@
+package main;
+
+import drivergenerator.WGStoredXSSDriverGenerator;
+
+
+public class Test {
+	
+	public static void main(String[] args) throws Exception{
+		WGStoredXSSDriverGenerator gen = new WGStoredXSSDriverGenerator();
+		gen.start();
+	}
+}
--- hit/src/main/Test.java
+++ hit/src/main/Test.java
@@ -1,12 +1,8 @@
 package main;
 
-import drivergenerator.WGStoredXSSDriverGenerator;
-
-
 public class Test {
 	
 	public static void main(String[] args) throws Exception{
-		WGStoredXSSDriverGenerator gen = new WGStoredXSSDriverGenerator();
-		gen.start();
+
 	}
 }
