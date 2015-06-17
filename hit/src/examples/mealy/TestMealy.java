691bb58ff1224e971e4929c0714d13b06202426b
Nicolas BREMOND <nicola.bremond@gmail.com>
2015-06-17 16:12:16 +0200

    add the exemple tried on blackboard

--- hit/src/examples/mealy/TestMealy.java
+++ hit/src/examples/mealy/TestMealy.java
@@ -8,22 +8,18 @@ public class TestMealy {
 
 	public static Mealy getAutomata() {
 		Mealy test = new Mealy("Test");
-		State s0 = test.addState(true);
-		State s1 = test.addState();
+		State s1 = test.addState(true);
 		State s2 = test.addState();
 		State s3 = test.addState();
 
-		test.addTransition(new MealyTransition(test, s0, s1, "a", "x"));
-		test.addTransition(new MealyTransition(test, s0, s3, "b", "x"));
+		test.addTransition(new MealyTransition(test, s1, s2, "a", "1"));
+		test.addTransition(new MealyTransition(test, s1, s3, "b", "1"));
 
-		test.addTransition(new MealyTransition(test, s1, s1, "a", "y"));
-		test.addTransition(new MealyTransition(test, s1, s2, "b", "x"));
+		test.addTransition(new MealyTransition(test, s2, s2, "a", "0"));
+		test.addTransition(new MealyTransition(test, s2, s1, "b", "0"));
 
-		test.addTransition(new MealyTransition(test, s2, s3, "a", "x"));
-		test.addTransition(new MealyTransition(test, s2, s3, "b", "x"));
-
-		test.addTransition(new MealyTransition(test, s3, s0, "a", "x"));
-		test.addTransition(new MealyTransition(test, s3, s0, "b", "x"));
+		test.addTransition(new MealyTransition(test, s3, s2, "a", "0"));
+		test.addTransition(new MealyTransition(test, s3, s3, "b", "1"));
 
 		return test;
 	}
