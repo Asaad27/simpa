f989d5e3bc2b9166b5741e67bcede67113421206
Lingxiao WANG <Lingxiao.wang@imag.fr>
2016-12-19 15:11:33 +0100

    add class NodeSplittingTree

--- hit/src/learner/mealy/tree/NodeSplittingTree.java
+++ hit/src/learner/mealy/tree/NodeSplittingTree.java
@@ -1,16 +1,23 @@
 package learner.mealy.tree;
 
+import java.util.ArrayList;
+
+import automata.mealy.InputSequence;
+import automata.mealy.OutputSequence;
 import learner.mealy.LmTrace;
 
 public class NodeSplittingTree extends LmTrace {
-	
+
+
 	public NodeSplittingTree(SplittingTree spTree) {
 		spTree.toString();
-		
+
 	}
+
 	public NodeSplittingTree() {
-		
-		
+
 	}
+
+	
 
 }
