49f5a23dae05f8cf48efc573aff8ff37c231519d
Nicolas BREMOND <nicolas.bremond@vertaco.eu>
2019-04-11 11:33:50 +0200

    Add comments for OptionValidator

--- src/options/OptionValidator.java
+++ src/options/OptionValidator.java
@@ -15,40 +15,115 @@ package options;
 import javax.swing.JLabel;
 import javax.swing.UIManager;
 
+/**
+ * This class enables developers to check the value of the options they use. The
+ * actual test should be written in the method {@link #check()} and they can
+ * provide messages to explain to the user why the value is not correct.
+ * 
+ * If the test needs to know which driver is used, have a look to
+ * {@link options.automataOptions.PostDriverValidator}
+ * 
+ * @author Nicolas BREMOND
+ *
+ */
 public abstract class OptionValidator {
+	/**
+	 * An enumeration of possible level of errors found in options.
+	 * 
+	 * The level will define the display of messages in graphical interface (see
+	 * {@link OptionValidator#createComponent()}) and define whether the
+	 * inference can be made or not with these options. Levels above
+	 * {@link #WARNING} abort inference.
+	 * 
+	 * @author Nicolas BREMOND
+	 */
 	public enum CriticalityLevel {
-		NOTHING, WARNING, ERROR,
+		/**
+		 * There is no problem, but a message can still be displayed.
+		 */
+		NOTHING,
+		/**
+		 * There is an issue, but we can still infer with these options.
+		 */
+		WARNING,
+		/**
+		 * The value of this option can not be used to start inference.
+		 */
+		ERROR,
+
 	}
 
 	private CriticalityLevel criticality = CriticalityLevel.NOTHING;
 	private String message = "";
 
-	public OptionValidator() {
-	}
-
+	/**
+	 * Get the {@link CriticalityLevel criticality} computed in last
+	 * {@link #check()}.
+	 * 
+	 * @return the {@link CriticalityLevel criticality} computed in last
+	 *         {@link #check()}.
+	 */
 	public CriticalityLevel getCriticality() {
 		return criticality;
 	}
 
+	/**
+	 * Set the {@link CriticalityLevel criticality} of the value of option.
+	 * 
+	 * @param criticality
+	 *            the {@link CriticalityLevel criticality} of the issue found by
+	 *            {@link #check()}.
+	 */
 	protected void setCriticality(CriticalityLevel criticality) {
 		this.criticality = criticality;
 	}
 
+	/**
+	 * Set the message explaining what is the issue to the user of SIMPA.
+	 * 
+	 * @param message
+	 *            a text to the user of SIMPA explaining what is the issue with
+	 *            the chosen value.
+	 */
 	protected void setMessage(String message) {
 		this.message = message;
 	}
 
+	/**
+	 * Reset criticality and remove message.
+	 */
 	protected void clear() {
 		setMessage("");
 		setCriticality(CriticalityLevel.NOTHING);
 	}
 
+	/**
+	 * Get the message computed in last {@link #check()}.
+	 * 
+	 * @return the message computed in last {@link #check()}.
+	 */
 	public String getMessage() {
 		return message;
 	}
 
+	/**
+	 * This method will be automatically called before starting inference and
+	 * when the value of option is changed.
+	 * 
+	 * Developers of sub-classes should write their test in this method and call
+	 * {@link #setCriticality(CriticalityLevel)} and {@link #setMessage(String)}
+	 * to set the result of their test (NB. it can be useful to start this
+	 * method to a call to {@link #clear()}).
+	 */
 	public abstract void check();
 
+	/**
+	 * Create the graphical component to display the message to the user of
+	 * SIMPA.
+	 * 
+	 * @return a graphical component containing the {@link #getMessage()
+	 *         message}.
+	 */
 	public JLabel createComponent() {
 		if (criticality.compareTo(CriticalityLevel.ERROR) >= 0)
 			return new JLabel(getMessage(),
